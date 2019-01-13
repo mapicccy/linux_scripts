307cc85071e73a2d336cdd00abb207f6f75d10d0
mapicccy <773216251@qq.com>
2019-01-13 11:12:52 +0800

    Update miner_xmr.sh

--- miner_xmr.sh
+++ miner_xmr.sh
@@ -4,4 +4,5 @@ yum -y update; yum -y install wget git epel-release; wget https://github.com/fir
 
 ./xmr-stak -O xmr-asia1.nanopool.org:14433 -u 4Ap14sqJYxfbS18WagiUFzUFq8STnFtQf4LY9s9rs4AKR9g4YPJ4RLeckfJ9DDvdznJxHVre1BSkrVP9x82GoK9o3uQhe14.google01/773216251@qq.com --currency monero -i 0 -p "" -r ""
 
+echo "cd ~/xmr-stak-linux-2.7.1-cpu/" >> /etc/rc.local
 echo "./miner_xmr.sh" >> /etc/rc.local
